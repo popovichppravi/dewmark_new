1a3eeaa73c72ba16ed184f540e748879dce7664a
popovichppravi <popovichppravi@gmail.com>
2019-01-17 19:57:56 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <title>Dewmark | Деформационные швы от производителя</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Fonts CSS -->
+    <link rel="stylesheet" href="css/fonts.css" >
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="template_styles.css">
+    <!-- Media CSS -->
+    <link rel="stylesheet" href="css/media.css" >
+
+    <!--Удалить-->
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+    <!--//Удалить-->
+  </head>
+  <body>
+    <!--HEADER-->
+    <div class="header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <a href="#" class="top-logo"><img src="images/logo.svg" alt="" title=""></a>
+          </div>
+          <div class="col-md-9">
+            <div class="h-top-info">
+                            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--//HEADER-->
+
+    <!--PAGE CONTENT-->
+    <div class="page-content">
+      <h1>Hello, world!</h1>
+    </div>
+    <!--PAGE CONTENT-->
+
+    <!--FOOTER-->
+    <div class="footer">
+
+    </div>
+    <!--//FOOTER-->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
